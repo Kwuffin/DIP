ba89619c6f329fe003b82d55cf3b6c7c63081006
Kwuffin <bestboykevin.bbg@gmail.com>
2021-05-14 15:23:05 +0200

    Mapper now gives lettercombination as output Mapper now changes all spaces to "#" Mapper now changes all other characters to "%"

--- Letterfrequnties/mapper.py
+++ Letterfrequnties/mapper.py
@@ -4,18 +4,37 @@ https://www.michael-noll.com/tutorials/writing-an-hadoop-mapreduce-program-in-py
 """
 
 import sys
+import re
 
+
+allowed = re.compile('[^a-zA-Z]')
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
-    # split the line into words
-    words = line.split()
-    # increase counters
-    for word in words:
+    for i, char in enumerate(line[:-1]):
+        char = char.lower()
+
+        # Check first letter
+        if char.isalpha():
+            s1 = char
+        else:
+            if char == ' ':
+                s1 = '#'
+            else:
+                s1 = '%'
+
+        # Check second letter
+        if line[i+1].isalpha():
+            s2 = line[i+1]
+        else:
+            if line[i+1] == ' ':
+                s2 = '#'
+            else:
+                s2 = '%'
         # write the results to STDOUT (standard output);
         # what we output here will be the input for the
         # Reduce step, i.e. the input for reducer.py
         #
         # tab-delimited; the trivial word count is 1
-        print('%s\t%s' % (word, 1))
+        print('%s-%s\t%s' % (s1, s2, 1))
