e42f105035f93166f3ad16774bf0f3dcb4b0a4a7
Kwuffin <bestboykevin.bbg@gmail.com>
2021-05-15 17:14:37 +0200

    Dataframe values now get changed to the ratio's at which the lettercombinations occur

--- Letterfrequnties/matrix_create.py
+++ Letterfrequnties/matrix_create.py
@@ -5,12 +5,19 @@ import pandas as pd
 all_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "#", "%"]
 
 df = pd.DataFrame(np.zeros((28, 28)), columns=all_letters, index=all_letters)
-print(df)
 
 for line in sys.stdin:
     line.strip()
 
     word, count = line.split("\t")
-    count = int(count.replace("\n", ""))
+    count = float(count.replace("\n", ""))
 
+    l1, l2 = word.split("-")
+    df.at[l1, l2] = count
 
+total_count = df.to_numpy().sum()
+
+for col in df:
+    df[col] = df[col].apply(lambda x: x/total_count)
+
+df.to_csv("out.csv")
